467f26a165d72b207b10c787d66e3bc32d589b07
Jonathan Lewis <jontyjlewis@gmail.com>
2022-05-09 16:26:49 -0700

    can drag journal

--- src/scenes/Desk.js
+++ src/scenes/Desk.js
@@ -5,9 +5,21 @@ class Desk extends Phaser.Scene {
 
     preload() {
         this.load.image("desk_Bg", './assets/desk.png');
+        this.load.image("journalCover", './assets/journalCover.png');
+        this.load.image("journal12", './assets/journal1-2.png');
+        this.load.image("journal34", './assets/journal3-4.png');
     }
     
     create() {
-        this.bg = this.add.tileSprite(0, 0, 1000, 600, 'desk_Bg').setOrigin(0, 0);
+        let bg = this.add.tileSprite(0, 0, 1000, 600, 'desk_Bg').setOrigin(0, 0);
+        let journal = this.physics.add.sprite(300, 250, 'journalCover').setOrigin(0, 0);
+        journal.setInteractive();
+        this.input.setDraggable(journal);
+
+        // need pointer for mouse coordinates to drag
+        this.input.on('drag', function (pointer, journal, dragX, dragY) {
+            journal.x = dragX;
+            journal.y = dragY;
+        });
     }
 }
