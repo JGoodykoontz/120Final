f1aa996405dd8eeb4a31429ae637957cb344ec09
Jonathan Lewis <jontyjlewis@gmail.com>
2022-05-09 14:56:54 -0700

    added basic desk scene

--- src/scenes/Desk.js
+++ src/scenes/Desk.js
@@ -3,7 +3,11 @@ class Desk extends Phaser.Scene {
         super("desk");
     }
 
+    preload() {
+        this.load.image("desk_Bg", './assets/desk.png');
+    }
+    
     create() {
-
+        this.bg = this.add.tileSprite(0, 0, 1000, 600, 'desk_Bg').setOrigin(0, 0);
     }
 }
