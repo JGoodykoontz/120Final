44e4d9aca125146024b40d392d4360af4b6df3c6
Jonathan Lewis <jontyjlewis@gmail.com>
2022-05-27 18:10:53 -0700

    finished journal

--- src/scenes/Cutscene.js
+++ src/scenes/Cutscene.js
@@ -16,6 +16,11 @@ class Cutscene extends Phaser.Scene {
             scene2 = this.add.image(0, 0, 'CS2Part2').setOrigin(0, 0).setDepth(2).setInteractive();
             scene3 = this.add.image(0, 0, 'CS2Part3').setOrigin(0, 0).setDepth(1).setInteractive();
         }
+        if(level == 3) {
+            scene1 = this.add.image(0, 0, 'CS2Part1').setOrigin(0, 0).setDepth(3).setInteractive();
+            scene2 = this.add.image(0, 0, 'CS2Part2').setOrigin(0, 0).setDepth(2).setInteractive();
+            scene3 = this.add.image(0, 0, 'CS2Part3').setOrigin(0, 0).setDepth(1).setInteractive();
+        }
 
         // play the cutscene on first time loading
         this.add.text(500, 550, "Click to continue", cutsceneConfig).setOrigin(0.5, 0.5).setDepth(5);   
